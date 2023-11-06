2e2d7a9e0620ee23994db171df13d929775460e7
agustinaboto <boto0705@gmail.com>
2023-11-06 02:01:07 -0300

    Favourites recycler view implementation

--- app/src/main/java/com/ort/edu/ar/parcialtp3/Fragments/FavouritesFragment.kt
+++ app/src/main/java/com/ort/edu/ar/parcialtp3/Fragments/FavouritesFragment.kt
@@ -1,45 +1,48 @@
 package com.ort.edu.ar.parcialtp3.Fragments
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.ort.edu.ar.parcialtp3.Adapters.DogListAdapter
+import com.ort.edu.ar.parcialtp3.Listener.OnViewItemClickedListener
 import com.ort.edu.ar.parcialtp3.R
-import com.ort.edu.ar.parcialtp3.ViewModels.FavouritesViewModel
-import com.ort.edu.ar.parcialtp3.databinding.FragmentFavouritesBinding
+import com.ort.edu.ar.parcialtp3.entities.Dog
 
-class FavouritesFragment : Fragment() {
-
-    private var _binding: FragmentFavouritesBinding? = null
-
-    private val binding get() = _binding!!
-
-    private lateinit var viewModel: FavouritesViewModel
+class FavouritesFragment : Fragment(), OnViewItemClickedListener {
+    private lateinit var dogListAdapter: DogListAdapter
+    lateinit var recDogs : RecyclerView
+    private lateinit var linearLayoutManager: LinearLayoutManager
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val FavouritesViewModel =
-            ViewModelProvider(this).get(FavouritesViewModel::class.java)
 
-        _binding =  FragmentFavouritesBinding.inflate(inflater, container, false)
-        val root: View = binding.root
+        val view = inflater.inflate(R.layout.fragment_favourites, container, false)
+        recDogs = view.findViewById(R.id.rec_dogs)
 
-        val textView: TextView = binding.textFavourites
-        FavouritesViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
-        return root
+        return view
+    }
+    override fun onStart() {
+        super.onStart()
+        val dogList = DogProvider.dogList.toMutableList()
+
+        requireActivity()
+        recDogs.setHasFixedSize(true)
+        linearLayoutManager = LinearLayoutManager(context)
+        dogListAdapter = DogListAdapter(dogList, this)
+        recDogs.layoutManager = linearLayoutManager
+        recDogs.adapter = dogListAdapter
+
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+    override fun onViewItemDetail(dog: Dog) {
+
     }
 
 }
